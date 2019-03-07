eee4ce867e6ca52ae82ba0e57a7d08933edc44e4
ogulcanertekin <ogulcanertekinn@gmail.com>
2019-03-07 05:34:44 +0300

    Password hash and save database  /register with bcryptjs module

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,35 @@
 const express = require('express');
 const router = express.Router();
+
+//Models
 const User=require('../models/User');
 
-/* GET home page. */
-router.get('/', (req, res, next)=> {
-  res.render('index', { title: 'Express' });
-});
+// Bcryptjs --> Parola veritabanına şifreli kaydetmek için modul--> npm install bcryptjs --save
+const bcrypt = require('bcryptjs');
 
 // /register [POST]
 
 router.post('/register', (req, res, next) => {
-  const { username, password } = req.body;
-
+	const { username, password } = req.body;
+	bcrypt.hash(password,10).then(function(hash){  //bcrypt.hash (password,sifreleme aralıgı)
 		const user = new User({
-			username,         //ES6 YAPISI destructing -->username=username demek.
-			password
+			username,         	//ES6 YAPISI destructing -->username=username demek.
+			password:hash  		//hash fonksiyonundan gelen hashlenmis parolayı password atadım.
 		});
 
-		const promise = user.save();
+		const promise = user.save();		//hashlenmis parola ve username veritabanına kaydediyoruz.
 		promise.then((data) => {
 			res.json(data)
 		}).catch((err) => {
 			res.json(err);
 		})
-	
+	});
 });
+
+
+/* GET home page. */
+router.get('/', (req, res, next)=> {
+	res.render('index', { title: 'Express' });
+  });
 
 module.exports = router;
